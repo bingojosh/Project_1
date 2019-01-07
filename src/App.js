caa0ab97377b024e084a53801f54b343e67896b8
Josh LaRochelle <joshua.r.larochelle@gmail.com>
2019-01-06 21:18:05 -0500

    Homepage Swag

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Search from "./Search.js";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/* <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -18,8 +18,10 @@ class App extends Component {
             rel="noopener noreferrer"
           >
             Learn React
-          </a>
+          </a> */}
+          <h1 className="title">MusicifyTube</h1>
         </header>
+          <Search />
       </div>
     );
   }
